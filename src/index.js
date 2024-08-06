2f3a733d3fdf6ae5ce1ce65dc37f33ddcb0221d7
famonte <faustowm@gmail.com>
2024-08-06 17:41:10 -0300

    Probando modificaciones

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ import { viewsRouter } from "./routes/views.router.js";
 import { Server } from "socket.io";
 import path from 'path';
 import { fileURLToPath } from 'url';
-
-
+import productManager from './managers/productManager.js';  // Importar productManager
 
 const PORT = 8080;
 const app = express();
@@ -21,6 +20,7 @@ app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 app.set("views", path.join(__dirname, "views"));
 
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -28,21 +28,21 @@ app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
 app.use("/", viewsRouter);
 
-
 const httpServer = app.listen(PORT, () => {
   console.log(`Escuchando en el puerto: ${PORT}`);
-})
+});
 
 const io = new Server(httpServer);
 
 io.on("connection", async (socket) => {
-  console.log("Un cliente se conecto");
-  
+  console.log("Un cliente se conectó");
+
   socket.emit("productos", await productManager.getProducts());
 
   socket.on("eliminarProducto", async (id) => {
-      await productManager.deleteProduct(id);
+    await productManager.deleteProduct(id);
 
-      io.socket.emit("productos", await productManager.getProducts);
-  })
-})
+    // Emitir el evento a todos los clientes conectados
+    io.emit("productos", await productManager.getProducts());
+  });
+});
